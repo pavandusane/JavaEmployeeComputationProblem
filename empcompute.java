bc6faa8cefba12d7300b72af4a51706953c30a97
pavandusane <dusanepv7@gmail.com>
2020-10-09 22:13:14 +0530

    Added : empcompute.java file added

---
+++ empcompute.java
@@ -0,0 +1,7 @@
+
+public class empcompute {
+
+	public static void main(String[] args) {
+		System.out.println("*************************Welcome to Employee Wage Computational Problem************************");
+}
+}
