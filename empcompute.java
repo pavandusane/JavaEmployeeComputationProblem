41eb462d1f742030e5544e69f0dc06ea0cc83305
pavandusane <dusanepv7@gmail.com>
2020-10-09 22:38:57 +0530

    Added : calculate part time wage

--- empcompute.java
+++ empcompute.java
@@ -7,6 +7,7 @@ public class empcompute {
 		Random r = new Random();
 		int wageperhour=20;
 		int fulldayhour=8;
+		int parttimehour=4;
 		int low = 1;
 		int high = 2;
 		int randomcheck = r.nextInt(high-low) + low;
@@ -14,6 +15,8 @@ public class empcompute {
 		System.out.println("Emplyee is Present");
 		int dailywage=(wageperhour*fulldayhour*randomcheck);
 	   System.out.println("DailyWage :"+dailywage);
+		int parttimewage=(wageperhour*parttimehour*randomcheck);
+		System.out.println("ParttimeWage :"+parttimewage);
 
 		}else{
 		System.out.println("Emplyee is absent");
