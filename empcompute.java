0df750777409bd0bb5d5f9786c939b59d856c4b9
pavandusane <dusanepv7@gmail.com>
2020-10-09 22:27:59 +0530

    Added : check emp present or absent

--- empcompute.java
+++ empcompute.java
@@ -1,7 +1,17 @@
+import java.util.Random;
 
 public class empcompute {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		System.out.println("*************************Welcome to Employee Wage Computational Problem************************");
+		Random r = new Random();
+		int low = 1;
+		int high = 2;
+		int randomcheck = r.nextInt(high-low) + low;					
+		if(randomcheck == 0){
+		System.out.println("Emplyee is Present");
+		}else{
+		System.out.println("Emplyee is absent");
+		}
 }
 }
