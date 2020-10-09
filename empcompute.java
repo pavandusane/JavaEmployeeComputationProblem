9ee81ed2fc8538d01959551137b5d4c3d83bb9db
pavandusane <dusanepv7@gmail.com>
2020-10-09 22:34:17 +0530

    Added : calculate daily wage

--- empcompute.java
+++ empcompute.java
@@ -5,11 +5,16 @@ public class empcompute {
 	public static void main(String[] args){
 		System.out.println("*************************Welcome to Employee Wage Computational Problem************************");
 		Random r = new Random();
+		int wageperhour=20;
+		int fulldayhour=8;
 		int low = 1;
 		int high = 2;
-		int randomcheck = r.nextInt(high-low) + low;					
-		if(randomcheck == 0){
+		int randomcheck = r.nextInt(high-low) + low;
+		if(randomcheck == 1){
 		System.out.println("Emplyee is Present");
+		int dailywage=(wageperhour*fulldayhour*randomcheck);
+	   System.out.println("DailyWage :"+dailywage);
+
 		}else{
 		System.out.println("Emplyee is absent");
 		}
